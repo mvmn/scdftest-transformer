6bb4dd47faa38223b33639af1e1d2cd2ec07fda3
Mykola Makhin <mvmn.inbox@gmail.com>
2020-04-23 11:40:45 +0300

    Added Lombok.

--- src/main/java/x/mvmn/study/scdf/scdftest/transformer/ScdfTestTransformerProperties.java
+++ src/main/java/x/mvmn/study/scdf/scdftest/transformer/ScdfTestTransformerProperties.java
@@ -2,16 +2,10 @@ package x.mvmn.study.scdf.scdftest.transformer;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Data;
+
 @ConfigurationProperties("mvmn-transformer")
+@Data
 public class ScdfTestTransformerProperties {
 	private String someConfigProp;
-
-	// TODO: use Lombok
-	public String getSomeConfigProp() {
-		return someConfigProp;
-	}
-
-	public void setSomeConfigProp(String someConfigProp) {
-		this.someConfigProp = someConfigProp;
-	}
 }
