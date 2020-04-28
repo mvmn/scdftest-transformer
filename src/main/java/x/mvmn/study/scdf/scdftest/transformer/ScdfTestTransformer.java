09461ec3e9c1ddbea5524595affbed3604a9a3a9
Mykola Makhin <mvmn.inbox@gmail.com>
2020-04-28 18:15:20 +0300

    Trying out Spring Cloud Function for SCDF transformer

--- src/main/java/x/mvmn/study/scdf/scdftest/transformer/ScdfTestTransformer.java
+++ src/main/java/x/mvmn/study/scdf/scdftest/transformer/ScdfTestTransformer.java
@@ -2,22 +2,20 @@ package x.mvmn.study.scdf.scdftest.transformer;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.binder.kafka.BinderHeaderMapper;
-import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.Transformer;
 import org.springframework.kafka.support.KafkaHeaderMapper;
 import org.springframework.messaging.Message;
 
 @SpringBootApplication
-@EnableBinding(Processor.class)
+// @EnableBinding(Processor.class)
 @EnableConfigurationProperties(ScdfTestTransformerProperties.class)
 @Configuration
 public class ScdfTestTransformer {
@@ -28,19 +26,24 @@ public class ScdfTestTransformer {
 	@Autowired
 	protected ScdfTestTransformerProperties config;
 
-	@Transformer(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
-	public Object transform(Message<?> message) {
-		Object payload = message.getPayload();
-		Map<String, Object> result = new HashMap<>();
-		Map<String, String> headersStr = new HashMap<>();
+	// @Transformer(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+	@Bean
+	public Function<Message<?>, Map<String, Object>> transform(
+	// Message<?> message
+	) {
+		return message -> {
+			Object payload = message.getPayload();
+			Map<String, Object> result = new HashMap<>();
+			Map<String, String> headersStr = new HashMap<>();
 
-		message.getHeaders().forEach((k, v) -> headersStr.put(k, v != null ? v.toString() : null));
+			message.getHeaders().forEach((k, v) -> headersStr.put(k, v != null ? v.toString() : null));
 
-		result.put("headers", headersStr);
-		result.put("payload", payload);
-		result.put("configProp", config.getSomeConfigProp());
+			result.put("headers", headersStr);
+			result.put("payload", payload);
+			result.put("configProp", "Config prop val: " + config.getSomeConfigProp());
 
-		return result;
+			return result;
+		};
 	}
 
 	// See https://stackoverflow.com/questions/59155689/could-not-decode-json-type-for-key-file-name-in-a-spring-cloud-data-flow-stream
